55fb6511b431d16735a312c90d5e7e3ed2b22780
anuprajwal <anuprajwal89@gmail.com>
2026-07-24 13:35:09 +0530

    allowed traffic

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,10 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 7000,
+    port: 7001,
     strictPort: true,
+    allowedHosts: [
+      'users.docapp.co.in'
+    ]
   }
 });
