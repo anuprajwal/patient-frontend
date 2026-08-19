5fc034b0e496ea57240b52ccc713cc118d1f1f3f
anuprajwal <anuprajwal89@gmail.com>
2026-08-20 00:58:01 +0530

    working on calling application

--- src/components/calling/VideoCallModal.jsx
+++ src/components/calling/VideoCallModal.jsx
@@ -2,7 +2,7 @@
 
 import React, { useEffect, useRef } from 'react';
 import { useCall } from '../../context/CallContext';
-import { PhoneOff } from '../ui/Icons';
+import { PhoneOff } from 'lucide-react';
 
 const formatSeconds = (sec) => {
   const m = Math.floor(sec / 60).toString().padStart(2, '0');
