6317a0ed394f416689d77b9a2926e4e4b134dbba
anuprajwal <anuprajwal89@gmail.com>
2026-08-20 00:46:28 +0530

    working on calling application

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -3,16 +3,19 @@
 importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging-compat.js');
 
-firebase.initializeApp({
+
+const firebaseConfig = {
   apiKey: "AIzaSyCE6uu63O91LA5eCfKKIz6n5_dHWm4nwpw",
   authDomain: "videocall-174e6.firebaseapp.com",
   projectId: "videocall-174e6",
   storageBucket: "videocall-174e6.firebasestorage.app",
   messagingSenderId: "965109245557",
   appId: "965109245557:web:eb5e5c760d3b41dbda7a3c",
-});
+};
 
 firebase.initializeApp(firebaseConfig);
+
+
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
